8eb949806072ae1da518e2fb50f1055a71e78483
liuxiaohua <lxh_aq@163.com>
2020-12-14 09:15:21 +0800

    添加lore工程

---
+++ CSRLore/LoreApi.cs
@@ -0,0 +1,250 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： BDSNetRunner
+ * 日期: 2020/12/8
+ * 时间: 11:06
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace CSRLore
+{
+	/// <summary>
+	/// 提供Lore对于nbt编辑的调用<br/>
+	/// 使用JavaScriptSerializer类进行序列化操作<br/>
+	/// 使用本类时，需要添加GAC引入 System.Web.Extensions，其它JSON工具类推
+	/// </summary>
+	public static class LoreApi
+	{
+		/// <summary>
+		/// 获取或添加一个value
+		/// </summary>
+		/// <param name="d">nbt</param>
+		/// <param name="k">待读取或追加的key值，可能是 tv 或 cv，其它key则无需追加填充左侧值</param>
+		/// <param name="leftk">追加时填充的左侧key值，可能是 tt 或 ck，空白则不填充此值</param>
+		/// <param name="leftv">左侧value值</param>
+		/// <returns></returns>
+		public static T getOrAppand<T>(Dictionary<string, object> d, string k, string leftk, object leftv) where T : class, new() {
+			if (d != null) {
+				object o;
+				if (d.TryGetValue(k, out o)) {
+					return o as T;
+				} else {
+					if (!string.IsNullOrEmpty(leftk))
+						d[leftk] = leftv;
+					var on = new T();
+					d[k] = on;
+					return on;
+				}
+			}
+			return default(T);
+		}
+
+		/// <summary>
+		/// 找到或添加一个cv
+		/// </summary>
+		/// <param name="l">包含nbt集合的列表，集合中总是由ck-cv组合构成</param>
+		/// <param name="k">待查找或追加的ck</param>
+		/// <returns></returns>
+		public static T findOrAppandCK<T>(ArrayList l, string k) where T : class, new() {
+			if (l != null) {
+				foreach(Dictionary<string, object> d in l) {
+					object ock = null;
+					if (d.TryGetValue("ck", out ock)) {
+						if (ock as string == k) {
+							object ocv = null;
+							if (d.TryGetValue("cv", out ocv))
+								return ocv as T;
+						}
+					}
+				}
+				var tag = new Dictionary<string, object>();
+				tag["ck"] = k;
+				var on = new T();
+				tag["cv"] = on;
+				l.Add(tag);
+				return on;
+			}
+			return default(T);
+		}
+		
+		/// <summary>
+		/// 找到并删除一个cv
+		/// </summary>
+		/// <param name="l">包含nbt集合的列表，集合中总是由ck-cv组合构成</param>
+		/// <param name="k">待删除的ck</param>
+		/// <returns></returns>
+		public static object findAndRemove(ArrayList l, string k) {
+			for (int m = l.Count; m > 0; --m) {
+				var data = l[m - 1] as Dictionary<string, object>;
+				if (data != null) {
+					object ock;
+					if (data.TryGetValue("ck", out ock)) {
+						string ck = ock as string;
+						if (ck == k) {
+							// 找到
+							l.Remove(data);
+							return data;
+						}
+					}
+				}
+			}
+			return null;
+		}
+		
+		/// <summary>
+		/// 找到物品类型
+		/// </summary>
+		/// <param name="item">以tt=10开头的物品nbt json</param>
+		/// <returns></returns>
+		public static string getItemType(Dictionary<string, object> item) {
+			string nid = string.Empty;
+			try {
+				var tv = getOrAppand<ArrayList>(item, "tv", "tt", 10);
+				if (tv != null) {
+					var namedata = findOrAppandCK<Dictionary<string, object>>(tv, "Name");
+					if (namedata != null) {
+						object onid;
+						if (namedata.TryGetValue("tv", out onid)) {
+							nid = onid as string;
+						}
+					}
+				}
+			} catch {}
+			return nid;
+		}
+		
+		/// <summary>
+		/// 获取关于lore标签的所有信息
+		/// </summary>
+		/// <param name="nbt">以tt=10开头的物品nbt json</param>
+		/// <returns>lore注释集合</returns>
+		public static ArrayList getLores(Dictionary<string, object> nbt) {
+			ArrayList oldtxtkv = null;
+				try {
+					var tv = getOrAppand<ArrayList>(nbt, "tv", "tt", 10);
+					if (tv != null) {
+						var tagdata = findOrAppandCK<Dictionary<string, object>>(tv, "tag");
+						if (tagdata != null) {
+							var taglist = getOrAppand<ArrayList>(tagdata, "tv", "tt", 10);
+							if (taglist != null) {
+								var displaydata = findOrAppandCK<Dictionary<string, object>>(taglist, "display");
+								if (displaydata != null) {
+									var displaylist = getOrAppand<ArrayList>(displaydata, "tv", "tt", 10);
+									if (displaylist != null) {
+										var loredata = findOrAppandCK<Dictionary<string, object>>(displaylist, "Lore");
+										if (loredata != null) {
+											var lorelist = getOrAppand<ArrayList>(loredata, "tv", "tt", 9);
+											if (lorelist != null) {
+												oldtxtkv = lorelist;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+			} catch{}
+			if (oldtxtkv != null && oldtxtkv.Count > 0) {
+				var ll = new ArrayList();
+				foreach(Dictionary<string, object> mlore in oldtxtkv) {
+					object olstr;
+					if (mlore.TryGetValue("tv", out olstr)) {
+						ll.Add(olstr as string);
+					}
+				}
+				return ll;
+			}
+			return null;
+		}
+		
+		/// <summary>
+		/// 删除lore标签
+		/// </summary>
+		/// <param name="item">以tt=10开头的物品nbt json</param>
+		public static void removeLores(Dictionary<string, object> item) {
+			try {
+				var tv = item["tv"] as ArrayList;
+					if (tv != null) {
+						var tagdata = findOrAppandCK<Dictionary<string, object>>(tv, "tag");
+						if (tagdata != null) {
+							var taglist = tagdata["tv"] as ArrayList;
+							if (taglist != null) {
+								var displaydata = findOrAppandCK<Dictionary<string, object>>(taglist, "display");
+								if (displaydata != null) {
+									var displaylist = displaydata["tv"] as ArrayList;
+									if (displaylist != null) {
+										findAndRemove(displaylist, "Lore");
+									}
+									if (displaylist.Count < 1)
+										displaydata.Clear();
+								}
+								if (displaydata.Count < 1)
+									findAndRemove(taglist, "display");
+							}
+							if (taglist.Count < 1)
+								tagdata.Clear();
+						}
+						if (tagdata.Count < 1)
+							findAndRemove(tv, "tag");
+					}
+			} catch{}
+		}
+
+		/// <summary>
+		/// 给当前物品添加注释
+		/// </summary>
+		/// <param name="item">以tt=10开头的物品nbt json</param>
+		/// <param name="txts">待注释文本集合，若为空集则删除lore标签</param>
+		/// <returns>是否成功添加</returns>
+		public static bool setLores(Dictionary<string, object> item, string [] txts) {
+			if (txts == null || txts.Length < 1) {
+				removeLores(item);
+				return true;
+			}
+			try {
+					var tv = getOrAppand<ArrayList>(item, "tv", "tt", 10);
+					if (tv != null) {
+						var tagdata = findOrAppandCK<Dictionary<string, object>>(tv, "tag");
+						if (tagdata != null) {
+							var taglist = getOrAppand<ArrayList>(tagdata, "tv", "tt", 10);
+							if (taglist != null) {
+								var displaydata = findOrAppandCK<Dictionary<string, object>>(taglist, "display");
+								if (displaydata != null) {
+									var displaylist = getOrAppand<ArrayList>(displaydata, "tv", "tt", 10);
+									if (displaylist != null) {
+										var loredata = findOrAppandCK<Dictionary<string, object>>(displaylist, "Lore");
+										if (loredata != null) {
+											var lorelist = getOrAppand<ArrayList>(loredata, "tv", "tt", 9);
+											if (lorelist == null) {
+												loredata["tv"] = new ArrayList();
+												lorelist = loredata["tv"] as ArrayList;
+											}
+											if (lorelist != null) {
+												// 创建新的lore
+												lorelist.Clear();
+												if (txts != null && txts.Length > 0) {
+													foreach (string txt in txts) {
+													var ntxt = new Dictionary<string, object>();
+													ntxt["tt"] = 8;
+													ntxt["tv"] = txt;
+													lorelist.Add(ntxt);
+													}
+												}
+												return true;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				} catch {}
+			return false;
+		}
+	}
+}
---
+++ CSRLore/MyClass.cs
@@ -0,0 +1,367 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： BDSNetRunner
+ * 日期: 2020/12/7
+ * 时间: 19:31
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+using CSR;
+
+namespace CSRLore
+{
+	/// <summary>
+	/// 自定义Lore注释
+	/// </summary>
+	public static class MyClass
+	{
+		static MCCSAPI mapi;
+		
+		const string CONFIG_DIR = @"CSR/lore";
+		const string CONFIG_PATH = @"CSR/lore/loreconfig.json";
+		
+		static Dictionary<string, object> config = new Dictionary<string, object>();
+		static string costtype = "level";
+		static string costsbname = "money";
+		static int costcount = 1;
+		static string tipsok = "[提示] 您的装备已添加注释成功。";
+		static string tipserr = "[出错] 装备添加注释失败。请检查命令是否正确，装备是否符合品级。";
+		static string tipscostly = "[失败] 过于昂贵，添加注释失败";
+		static string tipshelp = "[帮助] 输入 lore [注释信息] 指令，消耗一定经验或金钱以添加您装备的注释。";
+		static string tipshelpex = "[帮助] 输入 loreraw [JSON] 指令，消耗一定经验或金钱以添加您装备的注释。\n注：JSON中，信息集应包含在texts关键字对应的集合中。";
+		static ArrayList applyitems = new ArrayList();
+		
+		static readonly JavaScriptSerializer ser = new JavaScriptSerializer();
+		
+		const string djsonstr = "{\"cost\": \"level\",\n" +
+  "\"costname\": \"money\",\n" +
+  "\"count\": 1,\n" +
+  "\"tips\": {\n" +
+    "\"ok\": \"[提示] 您的装备已添加注释成功。\",\n"+
+    "\"error\": \"[出错] 装备添加注释失败。请检查命令是否正确，装备是否符合品级。\",\n"+
+    "\"costly\": \"[失败] 过于昂贵，添加注释失败\",\n"+
+    "\"help\": \"[帮助] 输入 lore [注释信息] 指令，消耗一定经验或金钱以添加您装备的注释。\",\n"+
+	"\"helpex\": \"[帮助] 输入 loreraw [JSON] 指令，消耗一定经验或金钱以添加您装备的注释。\\n注：JSON中，信息集应包含在texts关键字对应的集合中。\"\n" +
+  "},\n"+
+  "\"applyitems\": [\n"+
+	"\"minecraft:diamond_sword\",\n" +
+	"\"minecraft:netherite_sword\",\n" +
+	"\"minecraft:shield\",\n" +
+	"\"minecraft:elytra\"\n" +
+  "]\n"+
+"}";
+		
+		// 检查配置文件完整性
+		static bool checkConfigOK() {
+			object ot;
+			if (config.TryGetValue("cost", out ot)) {
+				costtype = ot as string;
+				if (costtype == null)
+					return false;
+				costtype = costtype.ToLower() == "level" ? "level" : "scoreboard";
+				object ona;
+				if (config.TryGetValue("costname", out ona)) {
+					costsbname = ona as string;
+					if (costsbname == null)
+						return false;
+					object occ;
+					if (config.TryGetValue("count", out occ)) {
+						costcount = Convert.ToInt32(occ);
+						object otips;
+						if (config.TryGetValue("tips", out otips)) {
+							var tips = otips as Dictionary<string, object>;
+							if (tips == null)
+								return false;
+							object otok;
+							if (tips.TryGetValue("ok", out otok)) {
+								tipsok = otok as string;
+								object oterr;
+								if (tips.TryGetValue("error", out oterr)) {
+									tipserr = oterr as string;
+									object otcostly;
+									if (tips.TryGetValue("costly", out otcostly)) {
+										tipscostly = otcostly as string;
+										object othelp;
+										if (tips.TryGetValue("help", out othelp)) {
+											tipshelp = othelp as string;
+											object othelpex;
+											if (tips.TryGetValue("helpex", out othelpex)) {
+												tipshelpex = othelpex as string;
+												object oits;
+												if (config.TryGetValue("applyitems", out oits)) {
+													applyitems = oits as ArrayList;
+													return applyitems != null;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			return false;
+		}
+		
+		static bool loadconfig() {
+			try {
+				Directory.CreateDirectory(CONFIG_DIR);
+				config = ser.Deserialize<Dictionary<string, object>>(File.ReadAllText(CONFIG_PATH));
+			}catch{}
+			if (config == null || config.Count < 1 || !checkConfigOK()) {
+				Console.WriteLine("[Lore] 配置文件读取失败，使用默认配置。");
+				try {
+					config = ser.Deserialize<Dictionary<string, object>>(djsonstr);
+					Directory.CreateDirectory(CONFIG_DIR);
+					File.WriteAllText(CONFIG_PATH, djsonstr);
+				}catch{}
+				return checkConfigOK();
+			}
+			return true;
+		}
+		
+		// 发送一个tellraw
+		public static void tellraw(string pname, string msg) {
+			var rawtxt = new Dictionary<string, object>();
+			var rawtext = new ArrayList();
+			var text = new Dictionary<string, object>();
+			text["text"] = msg;
+			rawtext.Add(text);
+			rawtxt["rawtext"] = rawtext;
+			mapi.runcmd("tellraw \"" + pname + "\" " + ser.Serialize(rawtxt));
+		}
+		
+		// 检查是否可支付注释费用
+		static bool checkCanpay(CsPlayer p) {
+			bool can = false;
+			switch(costtype) {
+				case "level":
+					try {
+							var plvl = ser.Deserialize<Dictionary<string, object>>(mapi.getPlayerAttributes(p.Uuid));
+							can = (Convert.ToInt32(plvl["level"])) - costcount >= 0;
+					} catch{}
+					break;
+				case "scoreboard":
+					try {
+						int d = mapi.getscoreboard(p.Uuid, costsbname);
+						can = d - costcount >= 0;
+					}catch{}
+					break;
+			}
+			return can;
+		}
+		// 支付等级或计分板数值
+		static bool pay(CsPlayer p) {
+			bool can = false;
+			switch(costtype) {
+				case "level":
+					try {
+							var plvl = ser.Deserialize<Dictionary<string, object>>(mapi.getPlayerAttributes(p.Uuid));
+							can = (Convert.ToInt32(plvl["level"])) - costcount >= 0;
+							if (can) {
+								var lvl = new Dictionary<string, object>();
+								lvl["level"] = (Convert.ToInt32(plvl["level"])) - costcount;
+								return mapi.setPlayerTempAttributes(p.Uuid, ser.Serialize(lvl));
+							}
+					} catch{}
+					break;
+				case "scoreboard":
+					try {
+						int d = mapi.getscoreboard(p.Uuid, costsbname);
+						int nd = d - costcount;
+						can = nd >= 0;
+						if (can) {
+							return mapi.runcmd("scoreboard players set \"" + p.getName() + "\" \"" + costsbname + "\" " + nd);
+						}
+					}catch{}
+					break;
+			}
+			return can;
+		}
+		
+		/// <summary>
+		/// 给玩家当前手持装备添加注释
+		/// </summary>
+		/// <param name="p">玩家类的实例</param>
+		/// <param name="txts">待注释文本集合</param>
+		/// <returns></returns>
+		public static int lore(CsPlayer p, string [] txts) {
+			// TODO 此处检查装备是否允许添加注释
+			int retcode = 0;		// 返回值：0 - err，1 - ok，2 - too costly
+			Dictionary<string, object> curitem = null;
+			try {
+				curitem = ser.Deserialize<Dictionary<string, object>>(mapi.getPlayerSelectedItem(p.Uuid));
+			} catch {}
+			if (curitem != null && curitem.Count > 0) {
+				bool isitemok = false;
+				try {
+					var data = LoreApi.getOrAppand<Dictionary<string, object>>(curitem, "selecteditem", "", "");
+					string nid = LoreApi.getItemType(data);
+					isitemok = !string.IsNullOrEmpty(nid) && 
+								(applyitems.Count < 1 || (applyitems.Count > 0 && applyitems.Contains(nid)));
+				} catch {}
+				if (!isitemok) {
+					return 0;
+				}
+				// TODO 此处进行扣分检查操作
+				if (!checkCanpay(p)) {
+					return 2;
+				}
+				// TODO 此处进行注释操作，成功后进行扣分操作
+				bool loreok = false;
+				try {
+					var data = LoreApi.getOrAppand<Dictionary<string, object>>(curitem, "selecteditem", "", "");
+					// test
+//					var oldlore = LoreApi.getLores(data);
+//					if (oldlore != null) {
+//						Console.WriteLine("[LOG] old lores =" + ser.Serialize(oldlore));
+//					} else {
+//						Console.WriteLine("[LOG] null old lores.");
+//					}
+					// test end
+					loreok = LoreApi.setLores(data, txts);
+				} catch {}
+				if (loreok) {
+					if (pay(p)) {
+						var data = LoreApi.getOrAppand<Dictionary<string, object>>(curitem, "selecteditem", "", "");
+						int slot = Convert.ToInt32(curitem["selectedslot"]);
+						var nal = LoreApi.getOrAppand<ArrayList>(data, "tv", "tt", 10);
+						var slotdata = LoreApi.findOrAppandCK<Dictionary<string, object>>(nal, "Slot");	// 添加背包中Slot标签以符合背包列表格式
+						slotdata["tt"] = 1;
+						slotdata["tv"] = slot;
+						var nitmdata = new Dictionary<string, object>();
+						var d = LoreApi.getOrAppand<ArrayList>(nitmdata, "tv", "tt", 9);
+						d.Add(data);
+						var nitms = new Dictionary<string, object>();
+						nitms["Inventory"] = nitmdata;
+						if (mapi.setPlayerItems(p.Uuid, ser.Serialize(nitms))) {
+							retcode = 1;
+						}
+					}
+				}
+			}
+			return retcode;
+		}
+		
+		// 根据返回码打印提示消息
+		static void tips(CsPlayer p, int code) {
+			switch(code) {
+				case 0:
+					tellraw(p.getName(), tipserr);
+					break;
+				case 1:
+					tellraw(p.getName(), tipsok);
+					break;
+				case 2:
+					tellraw(p.getName(), tipscostly);
+					break;
+				default:
+					// do nothing
+					break;
+			}
+		}
+		
+		// 插件主入口
+		public static void init(MCCSAPI api) {
+			mapi = api;
+			if (!loadconfig()) {
+				Console.WriteLine("[Lore] 配置文件读写失败，请检查插件是否配置正确。");
+				return;
+			}
+			api.addBeforeActListener(EventKey.onInputCommand, x => {
+			                         	var e = BaseEvent.getFrom(x) as InputCommandEvent;
+			                         	var mcmd = e.cmd.Trim();
+			                         	if (string.IsNullOrEmpty(mcmd))
+			                         		return false;
+			                         	if (mcmd == "/lore") {
+			                         		tellraw(e.playername, tipshelp);
+			                         		return false;
+			                         	}
+			                         	if (mcmd.IndexOf("/lore ") == 0) {
+			                         		CsPlayer p = new CsPlayer(api, e.playerPtr);
+			                         		int infocode = 0;
+			                         		var txt = mcmd.Substring(6);
+			                         		if (!string.IsNullOrEmpty(txt)) {
+			                         			// TODO 此处检查装备是否允许添加注释
+			                         			txt = "\n" + txt;
+			                         			infocode = lore(p, new string[] {txt});
+			                         		}
+			                         		tips(p, infocode);
+			                         		return false;
+			                         	}
+			                         	if (mcmd == "/loreraw") {
+			                         		tellraw(e.playername, tipshelpex);
+			                         		return false;
+			                         	}
+			                         	if (mcmd.IndexOf("/loreraw ") == 0) {
+			                         		CsPlayer p = new CsPlayer(api, e.playerPtr);
+			                         		int infocode = 0;
+			                         		var txts = new ArrayList();
+			                         		var jsonstr = mcmd.Substring(9).Trim();
+			                         		try {
+			                         			var data = ser.Deserialize<Dictionary<string, object>>(jsonstr);
+			                         			if (data != null) {
+			                         				object ot;
+			                         				if (data.TryGetValue("texts", out ot)) {
+			                         					txts = ot as ArrayList;
+			                         				}
+			                         			}
+			                         		} catch{}
+			                         		if (txts != null) {
+			                         			// TODO 此处检查装备是否允许添加注释
+			                         			try {
+			                         				infocode = lore(p, (string[])txts.ToArray(typeof(string)));
+			                         			}catch{}
+			                         		}
+			                         		tips(p, infocode);
+			                         		return false;
+			                         	}
+			                         	return true;
+			                         });
+			api.addBeforeActListener(EventKey.onServerCmd, x => {
+			                         	var e = BaseEvent.getFrom(x) as ServerCmdEvent;
+			                         	var mcmd = e.cmd.Trim();
+			                         	if (mcmd == "lore") {
+			                         		api.logout("[Lore] 用法（玩家）： /lore [说明]，/loreraw [说明集JSON]，（后台）：lore [reload]");
+			                         		return false;
+			                         	}
+			                         	if (mcmd.IndexOf("lore ") == 0) {
+			                         		var para = mcmd.Substring(5);
+			                         		if (!string.IsNullOrEmpty(para)) {
+			                         			if (para.Trim().ToLower() == "reload") {
+			                         				// TODO 此处重载配置文件
+			                         				if (!loadconfig()) {
+														api.logout("[Lore] 配置文件读写失败，请检查插件是否配置正确。");
+			                         				} else {
+			                         					api.logout("[Lore] 已成功重载配置文件。");
+			                         				}
+			                         			}
+			                         		}
+			                         		return false;
+			                         	}
+			                         	return true;
+			});
+			api.setCommandDescribe("lore", "自定义装备注释");
+			api.setCommandDescribe("loreraw", "自定义装备注释(JSON格式)");
+			Console.WriteLine("[Lore] 自定义装备注释指令已加载。用法（玩家）： /lore [说明]，/loreraw [说明集JSON]，（后台）：lore [reload]");
+		}
+	}
+}
+
+namespace CSR {
+	partial class Plugin{
+		public static void onStart(MCCSAPI api) {
+			if (api.COMMERCIAL) {
+				CSRLore.MyClass.init(api);
+			} else {
+				Console.WriteLine("[Lore] 暂不支持社区版。");
+			}
+		}
+	}
+}
