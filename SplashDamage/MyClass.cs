0ca57cb739f4236b9ce8cf00d12daf3e136a5144
zhkj-liuxiaohua <lxh_aq@163.com>
2021-07-23 11:13:19 +0800

    更新适配至1.17.10.04版

--- SplashDamage/MyClass.cs
+++ SplashDamage/MyClass.cs
@@ -23,19 +23,19 @@ namespace SplashDamage
 		static Hashtable swords = new Hashtable();
 		static JavaScriptSerializer ser = new JavaScriptSerializer();
 		
-		// 低版本 暂用id检测
-//		static void initSwordIds() {
-//			swords["wooden_sword"] = true;
-//			swords["stone_sword"] = true;
-//			swords["iron_sword"] = true;
-//			swords["diamond_sword"] = true;
-//			swords["golden_sword"] = true;
-//			swords["netherite_sword"] = true;
-//		}
+		// 高版本 用标识符检测
+		static void initSwordIds() {
+			swords["wooden_sword"] = true;
+			swords["stone_sword"] = true;
+			swords["iron_sword"] = true;
+			swords["diamond_sword"] = true;
+			swords["golden_sword"] = true;
+			swords["netherite_sword"] = true;
+		}
 		
 		public static void init(MCCSAPI api) {
 			mapi = api;
-			//initSwordIds();
+			initSwordIds();
 			api.addAfterActListener(EventKey.onAttack, x => {
 				var e = BaseEvent.getFrom(x) as AttackEvent;
 				if (e != null) {
@@ -46,9 +46,10 @@ namespace SplashDamage
 						var mainhand = hand[0] as Dictionary<string, object>;
 						if (mainhand != null) {
 							object oid;
-							if (mainhand.TryGetValue("id", out oid)) {
-								int id = Convert.ToInt32(oid);		// 剑
-								if (id == 308 || id == 312 || id == 307 || id == 322 || id == 316 || id == 592) {
+							if (mainhand.TryGetValue("rawnameid", out oid)) {
+								string rid = oid as string;		// 剑
+								var oisSword = swords[rid];
+								if (oisSword != null && (bool)oisSword) {
 									// TODO 此处执行溅射伤害操作
 									var pdata = a.Position;
 									var aXYZ = ser.Deserialize<Vec3>(a.Position);
@@ -83,34 +84,6 @@ namespace SplashDamage
 
 namespace CSR {
 	partial class Plugin {
-		private static MCCSAPI mapi = null;
-		/// <summary>
-		/// 静态api对象
-		/// </summary>
-		public static MCCSAPI api { get { return mapi; } }
-		public static int onServerStart(string pathandversion) {
-			string path = null, version = null;
-			bool commercial = false;
-			string [] pav = pathandversion.Split(',');
-			if (pav.Length > 1) {
-				path = pav[0];
-				version = pav[1];
-				commercial = (pav[pav.Length - 1] == "1");
-				mapi = new MCCSAPI(path, version, commercial);
-				if (mapi != null) {
-					onStart(mapi);
-					GC.KeepAlive(mapi);
-					return 0;
-				}
-			}
-			Console.WriteLine("Load failed.");
-			return -1;
-		}
-
-		~Plugin()
-        {
-			//Console.WriteLine("[CSR Plugin] Ref released.");
-        }
 
 		#region 必要接口 onStart ，由用户实现
 		public static void onStart(MCCSAPI api) {
